3b2d1475ef83f167bec2f706bfe4c196bf80cc73
lnf <naifengliu.us@gmail.com>
2021-04-12 12:27:17 -0400

    update 4/9/2021

--- dynprog.py
+++ dynprog.py
@@ -3,6 +3,7 @@ import numpy as np
 
 expression = lambda x : ((x[0] or x[1]) and (x[2] or x[3] or x[4] or x[5] or x[6])) or (x[7] or x[8])
 expression = "(x0 and x1) or (x2 or x3)"
+expression = "((x0 or x1) and (x2 or x3 or x4 or x5 or x6)) or x7 or x8"
 
 def resolved(expression, xL, gate):
     splitted = expression.split(gate)
@@ -17,7 +18,9 @@ def resolve(expression, L, sclasses, condition):
     return expression
 
 sclasses = [[0,1], [2,3]] # sibling-classes
+sclasses = [[0,1], [2,3,4,5,6],[7,8]] # sibling-classes
 T = [2, 2]
+T = [0,1,1]
 dtuple = T
 expressions = {}
 for L in range(len(sclasses)):
--- dynprog_modified.py
+++ dynprog_modified.py
@@ -36,8 +36,21 @@ class Gate:
 class Formula:
 	def __init__(self, gate: Gate):
 		self.f = gate
-		self.sibling_classes = [[], []]
+		self.sibling_classes = [[], [], []]
 		self.get_sibling_classes(self.f)
+		self.array_type = [[], []]
+		self.get_array_type(self.f)
+
+	def get_array_type(self, gate):
+		ret = [gate.gate_type]
+		for each in gate.variables:
+			ret.append(each)
+		self.array_type[0].append(ret)
+		self.array_type[1].append(gate)
+
+		for num in range(len(gate.variables)):
+			if gate.types[num] == 'gate':
+				self.get_array_type(gate.variables[num])
 
 	def get_sibling_classes(self, gate):
 		# print(gate.to_string())
@@ -50,9 +63,22 @@ class Formula:
 		if len(ret) > 0:
 			self.sibling_classes[0].append(ret)
 			self.sibling_classes[1].append(gate.gate_type)
+			self.sibling_classes[2].append(gate)
 
 	def show(self):
 		print(self.f.to_string())
+
+	def find_gate_contains_variable(self, variable):
+		for num in range(len(self.array_type[0])):
+			each = self.array_type[0][num]
+			if variable in each:
+				# return each
+				return [each, self.array_type[1][num]]
+		#
+		#
+		# for each in self.array_type[0]:
+		# 	if variable in each:
+		# 		return each
 
 
 gate_alpha = Gate('OR')
@@ -80,6 +106,7 @@ f = Formula(gate_delta)
 f.show()
 print()
 print(f.sibling_classes)
+print(f.array_type)
 
 I_max = [len(item) for item in f.sibling_classes[0]]
 
@@ -145,22 +172,28 @@ for m in range(1, max(I_max) + 1):
 		order = np.argsort(L_R_ratio)
 		print(L[order[-1]])
 
-		print(f.sibling_classes)
+		# print(f.sibling_classes)
 
-		for i in range(len(f.sibling_classes[0])):
-			gate = f.sibling_classes[0][i]
-			if L[order[-1]] in gate:
-				if f.sibling_classes[1][i] == "AND":
-					print(I[:i]+[I[i]]+I[i+1:])
-					print("TRUE")
-					print(I[:i] + [I[i]-1] + I[i + 1:])
-					print("FALSE")
-					print(I[:i] + [0] + I[i + 1:])
-				elif f.sibling_classes[1][i] == "OR":
-					print(I[:i]+[I[i]]+I[i+1:])
-					print("TRUE")
-					print(I[:i] + [0] + I[i + 1:])
-					print("FALSE")
-					print(I[:i] + [I[i]-1] + I[i + 1:])
+		gate_contains_first_test = f.find_gate_contains_variable(L[order[-1]])
 
-		print()
+		print(gate_contains_first_test)
+
+		if len(gate_contains_first_test) == 2:
+		# True Arc
+		# resolve gate to 1
+		# False Arc
+		# resolve gate to 0
+		elif len(gate_contains_first_test) > 2:
+			if gate_contains_first_test[0] == 'AND':
+				# True Arc
+				# reduce sibling class
+				# False Arc
+				# resolve gate to 0
+			elif gate_contains_first_test[0] == 'OR':
+				# True Arc
+				# resolve gate to 1
+				# False Arc
+				# reduce sibling class
+
+
+
