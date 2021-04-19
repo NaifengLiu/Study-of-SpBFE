27b0e0ced8c863f2cd734c81f4cd2e39df8644c1
R. Teal Witter <rtealwitter@gmail.com>
2021-04-19 18:29:41 -0400

    yay!!!

--- dynprog3.py
+++ dynprog3.py
@@ -49,68 +49,91 @@ def findhighestr(tree, costs, probs, dtuple):
             if sorted(ratios)[dtuple[sibnum]-1] > r:
                 r = sorted(ratios)[dtuple[sibnum]-1]
                 highpath = paths[sibnum] + [np.argsort(-np.array(ratios))[-dtuple[sibnum]]]
-                highi = sibnum
-    return highpath, highi
-
-            
-            
+                highsibnum = sibnum
+    return highpath, highsibnum
 
 
+def eradicatedescendants(tree, parentpath, dtuple):
+    classes, paths = getsiblings(tree, [])
+    for sibnum in range(len(dtuple)):
+        if paths[sibnum][:len(parentpath)] == parentpath:
+            dtuple = dtuple[:sibnum] + (0,) + dtuple[sibnum+1:]
+    return dtuple
+
+def livedescendants(tree, parentpath, dtuple):
+    classes, paths = getsiblings(tree, [])
+    numdescendants = 0
+    for sibnum in range(len(dtuple)):
+        if paths[sibnum][:len(parentpath)] == parentpath:
+            numdescendants += dtuple[sibnum]
+    return numdescendants > 0
+
+def resolvetrue(tree, parentpath, truetuple):
+    stoprecursion = parentpath == []
+    parent = getparent(tree, parentpath)
+    if parent['gate'] == 'OR':
+        truetuple = eradicatedescendants(tree, parentpath, truetuple)
+        if not stoprecursion:
+            truetuple = resolvetrue(tree, parentpath[:-1], truetuple)
+    elif parent['gate'] == 'AND':
+        if not livedescendants(tree, parentpath, truetuple):
+            if not stoprecursion:
+                truetuple = resolvetrue(tree, parentpath[:-1], truetuple)
+    return truetuple
+
+def resolvefalse(tree, parentpath, falsetuple):
+    stoprecursion = parentpath == []
+    parent = getparent(tree, parentpath)
+    if parent['gate'] == 'AND':
+        falsetuple = eradicatedescendants(tree, parentpath, falsetuple)
+        if not stoprecursion:
+            falsetuple = resolvefalse(tree, parentpath[:-1], falsetuple)
+    elif parent['gate'] == 'OR':
+        if not livedescendants(tree, parentpath, falsetuple):
+            if not stoprecursion:
+                falsetuple = resolvefalse(tree, parentpath[:-1], falsetuple)
+    return falsetuple
 
 #n = 5
 #alltreesn = trees.generatetrees(n)
 #tree = alltreesn[5][4]
+# GHJM Example
 tree = {'gate':'OR', 'children':['var', 'var', {'gate':'AND', 'children':[{'gate':'OR', 'children':['var', 'var']},{'gate':'OR', 'children':['var', 'var', 'var', 'var', 'var']}]}]}
 costs = [[1,1], [1,1,2,2,3], [], [1,1]]
 probs = [[.4,.3], [.45,.45,.9,.8,.6], [], [.7,.5]]
+# Toy Example
+tree = {'gate':'AND', 'children':[{'gate':'OR', 'children':['var', {'gate':'AND', 'children':['var', 'var']}]},{'gate':'OR', 'children':['var', 'var']}]}
+costs = [[1,2], [3], [1,2], []]
+probs = [[.4, .3], [.2], [.5, .7], []]
 
 classes, paths = getsiblings(tree, [])
 print(tree)
 print(classes)
+print(paths)
 sizeclasses = [len(lst) for lst in classes]
 reducedtrees = getreducedtrees(sizeclasses)
-cost = {}
-for dtuple in reducedtrees:
-    cost[dtuple] = np.Inf
-cost = {(0,)*len(sizeclasses):0}
+expectedcost = {}
+for m in reducedtrees:
+    for dtuple in reducedtrees[m]:
+        expectedcost[dtuple] = np.Inf
+expectedcost[(0,)*len(sizeclasses)] = 0
 
+for m in range(1,sum(sizeclasses)+1):
+    for dtuple in reducedtrees[m]:
+        highpath, highsibnum = findhighestr(tree, costs, probs, dtuple)
+        parentpath = highpath[:-1]
+        # TRUE ARC
+        truetuple = dtuple[:highsibnum] + (dtuple[highsibnum]-1,) + dtuple[highsibnum+1:]
+        truetuple = resolvetrue(tree, parentpath, truetuple)
 
-#for m in range(1,sum(sizeclasses)+1):
-#    for dtuple in reducedtrees[m]:
-#        print(dtuple)
+        # FALSE ARC
+        falsetuple = dtuple[:highsibnum] + (dtuple[highsibnum]-1,) + dtuple[highsibnum+1:]
+        falsetuple = resolvefalse(tree, parentpath, falsetuple)
 
-dtuple = (2,3,0,0)
-highpath, highi = findhighestr(tree, costs, probs, dtuple)
-# TRUE ARC
-truetuple = copy(dtuple)
-#parent = highpath[:-1]
-#if parent['gate'] == 'OR':
-#    truetuple[highi] = 0
-#    for sibnum in range(len(dtuple)):
-#        if paths[sibnum][:len(parent)] == parent:
-#            truetuple[sibnum] = 0
-    
+        prob = probs[highsibnum][highpath[-1]]
+        cost = costs[highsibnum][highpath[-1]]
+        candidate = cost + prob * expectedcost[truetuple] + (1-prob)*expectedcost[falsetuple]
+        if candidate < expectedcost[dtuple]:
+            expectedcost[dtuple] = candidate
 
-# parent and, false arc
-#dtuple2tree(tree, dtuple)
-
-
-
-#I_max = [len(item) for item in f.sibling_classes[0]]
-#I_dict = dict()
-#
-#for i in range(max(I_max) + 1):
-#	I_dict[i] = []
-#
-#I_dict[0] = [[0] * len(I_max)]
-#
-#for i in range(1, max(I_max) + 1): #I_max = [1,3,5]
-#	for item in I_dict[i - 1]:
-#		# print(item)
-#		for j in range(len(I_max)):
-#			if item[j] + 1 <= I_max[j]:
-#				I_dict[i].append(item[:j] + [item[j] + 1] + item[j + 1:])
-#	I_dict[i].sort()
-#	I_dict[i] = list(I_dict[i] for I_dict[i], _ in itertools.groupby(I_dict[i]))  # remove duplicates
-#
-#print(I_dict)
+print(expectedcost)
